4ff4af49b47790d2359680afdbd11c3ff4c8de16
BraydenTW <adamhamadilebras@gmail.com>
2025-10-01 10:50:55 +0200

    Update favicon to ADAMSITE LOGO and change title to Adam - Developper, Designer, Creator

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,22 +5,11 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/static/favicon/apple-touch-icon.png"
-          />
+          <title>Adam - Developper, Designer, Creator</title>
           <link
             rel="icon"
             type="image/png"
-            sizes="32x32"
-            href="/static/favicon/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/static/favicon/favicon-16x16.png"
+            href="/static/logos/ADAMSITE LOGO (1).png"
           />
           <link rel="manifest" href="/static/favicon/site.webmanifest" />
         </Head>
