d70de523a74dd3dbff97bcc8b301ae92517b611a
BraydenTW <adamhamadilebras@gmail.com>
2025-10-11 13:20:27 +0200

    Add Tawakkul Académie and Tawakkul Voyage projects

--- data/content/projects.ts
+++ data/content/projects.ts
@@ -1,7 +1,24 @@
 import { kebabCase, kebabArray } from "@/utils/utils";
 import { Project } from "types";
 
-const projects: Project[] = [];
+const projects: Project[] = [
+  {
+    id: 0,
+    title: "Tawakkul Académie",
+    desc: "Plateforme dédiée à l'excellence spirituelle et académique islamique avec formations, communauté et valeurs fondamentales.",
+    img: "/static/projects/sitetawakkulacademievideo.mp4",
+    link: "https://tawakkulacademie.netlify.app/#accueil",
+    tags: ["HTML", "CSS", "Javascript", "Spiritualité"],
+  },
+  {
+    id: 1,
+    title: "Tawakkul Voyage",
+    desc: "Site web pour voyages spirituels et retraites avec réservation en ligne et informations sur les destinations.",
+    img: "/static/projects/tawakkulvoyages.png",
+    link: "https://tawakkulvoyage.netlify.app/#reservation",
+    tags: ["HTML", "CSS", "Javascript", "Voyage"],
+  },
+];
 
 export const allTags = []
 
